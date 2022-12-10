336f30414aa494a0c23927b7bd93b97b1c309d36
Bryanwebprogrammer2012 <119443381+Bryanwebprogrammer2012@users.noreply.github.com>
2022-12-10 15:48:58 +0300

    Add files via upload

--- Guess game update.py
+++ Guess game update.py
@@ -1,4 +1,4 @@
-import random
+import random 
 
 attempts = 0
 
@@ -10,15 +10,23 @@ guess = int(input("Guess a number between 1 and 100: "))
 
 
 while guess != number:
+    if guess>100:
+        print("You are not allowed to put a number greater than 100 ")
+        guess = int(input("\nGuess a number between 1 and 100: "))
+    if guess<0:
+        print("You are not allowed to put a number less than 0 ")
+        guess = int(input("\nGuess a number between 1 and 100: "))
     if guess < number:
         print("Your guess was too low.")
         attempts = attempts + 1
         guess = int(input("\nGuess a number between 1 and 100: "))
-    
+   
     else:
-        print("Your guess was too high.")
+        print("your guess is too high")
         attempts = attempts + 1
         guess = int(input("\nGuess a number between 1 and 100: "))
+    
+    
 
 print("You guessed correctly!")
 print("You guessed the answer in", attempts, "attempts.")       
